cf3956486dc80baa8965cc2337abb2661dbc4621
AntonioSoto01 <asotocid@gmail.com>
2023-12-12 21:44:30 +0100

    separando logica

--- src/main/java/com/antonio/hundirlaflota/Controladores/UsuarioController.java
+++ src/main/java/com/antonio/hundirlaflota/Controladores/UsuarioController.java
@@ -1,41 +1,53 @@
-/*
- * Copyright 2012-2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.antonio.hundirlaflota.Controladores;
 
 import com.antonio.hundirlaflota.Modelos.Usuario;
 import com.antonio.hundirlaflota.Repositorios.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@CrossOrigin(origins = "${frontend.url}")
 public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @Value("${frontend.url}")
+    String frontendUrl;
+
     @RequestMapping("/user")
     public Usuario user(@AuthenticationPrincipal OAuth2User principal) {
         Usuario usuario = new Usuario();
-        System.out.println(principal);
-        usuario.setNombre(principal.getAttribute("name"));
-        usuario.setEmail(principal.getAttribute("email"));
-        usuarioRepository.save(usuario);
-        System.out.println("usuario" + usuario);
+        
+        if (principal != null) {
+            System.out.println(principal);
+            String name = principal.getAttribute("name");
+            String email = principal.getAttribute("email");
+            
+            // Check if name and email are not null before setting them
+            if (name != null) {
+                usuario.setNombre(name);
+            }
+            if (email != null) {
+                usuario.setEmail(email);
+            }
+            
+            usuarioRepository.save(usuario);
+            System.out.println("usuario" + usuario);
+        } else {
+            // Handle the case where principal is null (optional based on your requirements)
+            // For example:
+            throw new RuntimeException("Principal is null"); // You can customize this exception or handle it differently
+        }
+        
         return usuario;
     }
+  
+
 }
--- src/main/java/com/antonio/hundirlaflota/SecurityConfiguration.java
+++ src/main/java/com/antonio/hundirlaflota/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.antonio.hundirlaflota;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,31 +13,54 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/index.html", "/", "/home", "/login", "/api/**","/**.css", "/**.js").permitAll()
-                                .requestMatchers( HttpMethod.POST, "/api/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .exceptionHandling(exceptionHandling ->
-                        exceptionHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                )
-                .csrf().disable()
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                        .requestMatchers("/**","/index.html", "/", "/home", "/login", "/api/**", "/user", "/oauth2/**", "/**.css", "/**.js").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
+                        
+        )
+        .exceptionHandling(exceptionHandling ->
+                exceptionHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        )
+        .csrf(csrf -> csrf.disable())
                 .logout(logout ->
                         logout.logoutSuccessUrl("/").permitAll()
                 )
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login(oauth2 -> oauth2
+                .defaultSuccessUrl(frontendUrl+"?success=true")
+                .failureUrl(frontendUrl+"?fail=true")
+        );
         return http.build();
     }
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(""));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
